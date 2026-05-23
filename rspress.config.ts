faf76d073d5a19e28a0d01dc04ae8cf22f790c03
yimimuzi <168008006+yimimuzi@users.noreply.github.com>
2026-05-23 23:44:25 +0800

    Update rspress.config.ts

--- rspress.config.ts
+++ rspress.config.ts
@@ -7,9 +7,12 @@ export default defineConfig({
   description: '一米距离，无限生态 —— 让服务与科技，近在咫尺。',
   icon: 'https://lf3-static.bytednsdoc.com/obj/eden-cn/uhp_hq7vwp/rspress/rspress-logo.png',
   themeConfig: {
+    // ⬇️ 就是这行，成功帮你关闭右上角的 Search (Ctrl K) 搜索框
+    search: false,
+
     nav: [
       { text: '首页', link: '/' },
-      { text: '指南', link: '/guide/getting-started' }
+      { text: '业务', link: '/guide/getting-started' }
     ],
     sidebar: {
       '/guide/': [
